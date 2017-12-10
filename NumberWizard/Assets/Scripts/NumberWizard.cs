300fc05d6d4647933d2138259093ce443bd36078
Neous1 <Neous1@users.noreply.github.com>
2017-12-10 13:33:07 -0500

    reset the guess variable

--- NumberWizard/Assets/Scripts/NumberWizard.cs
+++ NumberWizard/Assets/Scripts/NumberWizard.cs
@@ -2,19 +2,23 @@
 using System.Collections;
 
 public class NumberWizard : MonoBehaviour {
+	//make min nad max global
+
+	int max = 1000;
+	int min = 1;
+	int guess = 500; 
 
 	// Use this for initialization
 	void Start () {
 		print("Welcome to Number Wizard");
 		print("Pick a number in your head, but don't tell me !");
 
-		int max = 1000;
-		int min = 1;
+
 
 		print("The highest number you can pick is " + max);
 		print("the lowest number you can pick is " + min);
 
-		print("Is the number higher  or lower than 500?");
+		print("Is the number higher  or lower than " + guess);
 		print("Up = higher, down = lower , return = equal");
 
 			
@@ -24,6 +28,12 @@ public class NumberWizard : MonoBehaviour {
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.UpArrow)) {
 			print ("Up arrow pressed");		
+			//set guess to min
+			min = guess;
+			//reset guess
+			guess = (min + max)/2;
+			print(guess, max);
+
 		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
 			print ("Up down pressed");	
 		} else if (Input.GetKeyDown (KeyCode.Return)) {
