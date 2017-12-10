96298917388fbac88e9e863ec2c15594f2ea42c6
Neous1 <Neous1@users.noreply.github.com>
2017-12-10 13:09:17 -0500

    prompt user for number

--- NumberWizard/Assets/Scripts/NumberWizard.cs
+++ NumberWizard/Assets/Scripts/NumberWizard.cs
@@ -5,11 +5,29 @@ public class NumberWizard : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		print("Welcome to Number Wizard");
+		print("Pick a number in your head, but don't tell me !");
+
+		int max = 1000;
+		int min = 1;
+
+		print("The highest number you can pick is " + max);
+		print("the lowest number you can pick is " + min);
+
+		print("Is the number higher  or lower than 500?");
+		print("Up = higher, down = lower , return = equal");
+
+			
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			print ("Up arrow pressed");		
+		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			print ("Up down pressed");	
+		} else if (Input.GetKeyDown (KeyCode.Return)) {
+			print ("I won!");	
+		}
 	}
 }
