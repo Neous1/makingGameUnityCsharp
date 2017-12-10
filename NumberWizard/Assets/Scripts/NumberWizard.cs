d8d274df21fb6e10ba17bcee5dc8940a510ebafc
Neous1 <Neous1@users.noreply.github.com>
2017-12-10 13:53:15 -0500

    Create new method StartGame and move start() code into it call startGame from Start()

--- NumberWizard/Assets/Scripts/NumberWizard.cs
+++ NumberWizard/Assets/Scripts/NumberWizard.cs
@@ -9,20 +9,23 @@ public class NumberWizard : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+
+			
+	}
+	void StartGame(){
 		//fix the rounding problem to retun max
 		max = max + 1;
 		print("Welcome to Number Wizard");
 		print("Pick a number in your head, but don't tell me !");
-
-
-
+		
+		
+		
 		print("The highest number you can pick is " + max);
 		print("the lowest number you can pick is " + min);
-
+		
 		print("Is the number higher  or lower than " + guess);
 		print("Up = higher, down = lower , return = equal");
-
-			
+	
 	}
 	
 	// Update is called once per frame
