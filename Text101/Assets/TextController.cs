ec59b9a8da72eb65a6023f725ee5c437236fa02b
Neous1 <Neous1@users.noreply.github.com>
2017-12-11 04:14:29 -0500

    change text on keypress

--- Text101/Assets/TextController.cs
+++ Text101/Assets/TextController.cs
@@ -14,6 +14,8 @@ public class TextController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			text.text = "Space key pressed";
+		}
 	}
 }
