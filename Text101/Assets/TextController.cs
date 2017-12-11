3e5a62f0110959cd3e610e26fb65c54ac21f179f
Neous1 <Neous1@users.noreply.github.com>
2017-12-11 04:45:43 -0500

    build the story

--- Text101/Assets/TextController.cs
+++ Text101/Assets/TextController.cs
@@ -8,19 +8,19 @@ public class TextController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		text.text = "Hello World!";
-	
+		text.text = "Hello World!"; 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.Space)) {
-			text.text = "Lorem ipsum dolor sit amet consectetur adipiscing elit praesent himenaeos, " +
-						"pharetra a eros enim euismod maecenas integer. Penatibus dis vel magna semper " +
-						"montes consequat posuere et eu per, viverra libero vivamus rhoncus porta fringilla " +
-						"cum auctor scelerisque tellus vehicula, platea rutrum torquent nam cursus leo tempus " +
-						"curae neque. Curae dapibus libero magnis ultrices risus euismod leo donec, non lobortis " +
-						"sapien pharetra cum ornare cras fames, tempus torquent potenti sollicitudin lectus habitant taciti.";
+			text.text = "You are in a prison cell and you want to escape. \n"+
+						"There are some dirty sheets on the bed, \n"+
+						"a mirror on the wall, \n"+
+						"a door locked from the outside.\n\n" +
+						"Press S to view sheets, M to view Mirror, and L to view Lock";
+						
+
 		}
 	}
 }
