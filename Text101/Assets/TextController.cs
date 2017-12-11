505de0ac4d5073288393d1e4befa46805052a307
Neous1 <Neous1@users.noreply.github.com>
2017-12-11 04:53:59 -0500

    declare States of type enum create a new method to depict the state

--- Text101/Assets/TextController.cs
+++ Text101/Assets/TextController.cs
@@ -5,6 +5,7 @@ using System.Collections;
 public class TextController : MonoBehaviour {
 	//add a text variable of type text 
 	public Text text;
+	private enum States {cell, mirror, sheets_0, lock_0, cell_mirror, sheets_1, lock_1, freedom};
 
 	// Use this for initialization
 	void Start () {
@@ -13,14 +14,17 @@ public class TextController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.Space)) {
-			text.text = "You are in a prison cell and you want to escape. \n"+
-						"There are some dirty sheets on the bed, \n"+
-						"a mirror on the wall, \n"+
-						"a door locked from the outside.\n\n" +
-						"Press S to view sheets, M to view Mirror, and L to view Lock";
-						
-
-		}
+//		if (Input.GetKeyDown (KeyCode.Space)) {
+//
+//						
+//
+//		}
+	}
+	void State_cell(){
+		text.text = "You are in a prison cell and you want to escape. \n"+
+			"There are some dirty sheets on the bed, \n"+
+				"a mirror on the wall, \n"+
+				"a door locked from the outside.\n\n" +
+				"Press S to view sheets, M to view Mirror, and L to view Lock";
 	}
 }
